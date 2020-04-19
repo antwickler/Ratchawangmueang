1ef1f41075eaebb49f40688681bb2cf3b2180496
Weerasak Ratchawangmueang <weerasak.ratchawangmueang@gmail.com>
2020-04-20 01:40:13 +0700

    1st updated 20-04-20

--- css/extension.js
+++ css/extension.js
@@ -1,65 +1,16 @@
-// Scroll to top
-var scrolltotop={
-    setting:{
-        startline:100,scrollto:0,scrollduration:1e3,fadeduration:[500,100]
-    },
-    controlHTML:'<img src="https://i1155.photobucket.com/albums/p559/scrolltotop/arrow3.png" />',
-    controlattrs:{
-        offsetx:15,
-        offsety:15
-    },
-    anchorkeyword:"#top",
-    state:{
-        isvisible:!1,
-        shouldvisible:!1
-    },
-
-    scrollup:function(){
-        this.cssfixedsupport||this.$control.css({opacity:0});
-        var t=isNaN(this.setting.scrollto)?this.setting.scrollto:parseInt(this.setting.scrollto);
-        t="string"==typeof t&&1==jQuery("#"+t).length?jQuery("#"+t).offset().top:0,
-        this.$body.animate({scrollTop:t},this.setting.scrollduration)
-    },
-    
-    keepfixed:function(){
-        var t=jQuery(window),
-        o=t.scrollLeft()+t.width()-this.$control.width()-this.controlattrs.offsetx,
-        s=t.scrollTop()+t.height()-this.$control.height()-this.controlattrs.offsety;
-        this.$control.css({left:o+"px",top:s+"px"})
-    },
-    
-    togglecontrol:function(){
-        var t=jQuery(window).scrollTop();
-        this.cssfixedsupport||this.keepfixed(),this.state.shouldvisible=t>=this.setting.startline?!0:!1,
-        this.state.shouldvisible&&!this.state.isvisible?(this.$control.stop().animate({opacity:1},
-        this.setting.fadeduration[0]),
-        this.state.isvisible=!0):0==this.state.shouldvisible&&this.state.isvisible&&(this.$control.stop().animate({opacity:0},
-        this.setting.fadeduration[1]),this.state.isvisible=!1)
-    },
-    
-    init:function(){jQuery(document).ready(function(t){
-        var o=scrolltotop,
-        s=document.all;o.cssfixedsupport=!s||s&&"CSS1Compat"==document.compatMode&&window.XMLHttpRequest,
-        o.$body=t(window.opera?"CSS1Compat"==document.compatMode?"html":"body":"html,body"),
-        o.$control=t('<div id="topcontrol">'+o.controlHTML+"</div>").css({position:o.cssfixedsupport?"fixed":"absolute",
-        bottom:o.controlattrs.offsety,right:o.controlattrs.offsetx,opacity:0,
-        cursor:"pointer"}).attr({title:"Scroll to Top"}).click(function(){return o.scrollup(),!1}).appendTo("body"),
-        document.all&&!window.XMLHttpRequest&&""!=o.$control.text()&&o.$control.css({width:o.$control.width()}),
-        o.togglecontrol(),t('a[href="'+o.anchorkeyword+'"]').click(function(){return o.scrollup(),!1}),
-        t(window).bind("scroll resize",function(t){o.togglecontrol()})})
-    }
-};
-
-scrolltotop.init();
-// End Scroll to top
-
 // Do not click right
-function clickIE() {if (document.all) {alert(message);return false;}}
-function clickNS(e) {
-    if (document.layers||(document.getElementById&&!document.all)) {
-    if (e.which==2||e.which==3) {alert(message);return false;}}
-}
-if (document.layers) {document.captureEvents(Event.MOUSEDOWN);document.onmousedown=clickNS;}
-else {document.onmouseup=clickNS;document.oncontextmenu=clickIE;}
-document.oncontextmenu=new Function("return false")
-// End Do not click right
+
+document.oncontextmenu = new Function("return false;");
+
+
+// Google Analytics
+
+(function (i, s, o, g, r, a, m) {
+  i['GoogleAnalyticsObject'] = r; i[r] = i[r] || function () {
+    (i[r].q = i[r].q || []).push(arguments)
+  }, i[r].l = 1 * new Date(); a = s.createElement(o),
+  m = s.getElementsByTagName(o)[0]; a.async = 1; a.src = g; m.parentNode.insertBefore(a, m)
+})(window, document, 'script', 'https://www.google-analytics.com/analytics.js', 'ga');
+
+ga('create', 'UA-23216507-1', 'auto');
+ga('send', 'pageview');
